e660d19b0e1956e2ccdd74c84f081a8c523255c2
Rchl04 <racheliaachmara@gmail.com>
2026-01-03 22:12:02 +0700

    fix: mlflow autolog

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,7 +8,6 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import mlflow
 import mlflow.sklearn
 
-mlflow.set_experiment("Insurance Regression")
 mlflow.sklearn.autolog()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -31,10 +30,3 @@ models = {
 for name, model in models.items():
         model.fit(X_train, y_train)
         preds = model.predict(X_test)
-
-        mlflow.log_param("model_type", name)
-        mlflow.log_metric("MAE", mean_absolute_error(y_test, preds))
-        mlflow.log_metric("RMSE", np.sqrt(mean_squared_error(y_test, preds)))
-        mlflow.log_metric("R2", r2_score(y_test, preds))
-        
-        mlflow.sklearn.log_model(model, "model")
